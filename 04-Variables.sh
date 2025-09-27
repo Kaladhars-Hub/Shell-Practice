876fa72fa2a7021f54b802a8e3cf566a84ff3f4e
Kaladhars-Hub <kaladharcareers@gmail.com>
2025-09-27 20:33:35 +0530

    Variabales

---
+++ 04-Variables.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+# No space after or before equal
+PERSON1=Trump
+PERSON2=Putin
+
+echo "$PERSON1:: Welcome to Alaska, Mr. $PERSON2! How was your journey?"
+echo "$PERSON2:: Thank you, Mr. $PERSON1. The flight was long but comfortable. Alaska is beautiful."
+echo "$PERSON1:: We're glad to have you here for these important discussions."
+echo "$PERSON2:: Yes, these talks are crucial for our nations, Mr. $PERSON1."
+echo "$PERSON1:: Let's work together for peace and prosperity."
+echo "$PERSON2:: I agree, Mr. $PERSON1. Cooperation benefits both our countries."
