f1a57c29ce032dc2dde2e64dd3af44366a3404a8
Kaladhars-Hub <kaladharcareers@gmail.com>
2025-10-04 23:42:25 +0530

    Special Variables

---
+++ 08-Special_Variables.sh
@@ -0,0 +1,9 @@
+#!bin/bash
+
+echo "Script name: $0"           # Script filename
+echo "First argument: $1"        # First parameter
+echo "Second argument: $2"       # Second parameter
+echo "All arguments: $@"         # All parameters
+echo "Number of arguments: $#"   # Count of parameters
+echo "Exit status: $?"           # Last command's exit code
+echo "Process ID: $$"            # Current script's PID
