b304271c7dc59633f9cf98c4a9c83bd9bc01cdc7
Kaladhars-Hub <kaladharcareers@gmail.com>
2025-10-04 17:20:16 +0530

    Learning basics of Variables

---
+++ 06-Variables.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+# Basic variable (no spaces around =)
+name="john"
+age=25
+city="New York"
+
+#Using variables (add $ before name)
+echo "my name is $name"
+echo "I am $age years old"
+echo "I live in $city"
+
+# Another way to use variables
+echo "Hello, ${name}!" #Curly braces for clarity
