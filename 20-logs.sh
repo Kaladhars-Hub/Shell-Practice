10d15e59debe6fbd9ce47747bf84a42e3395c8d1
Kaladhars-Hub <kaladharcareers@gmail.com>
2025-10-08 19:56:10 +0530

    Learning about logs

--- 20-logs.sh
+++ 20-logs.sh
@@ -1,57 +1,49 @@
 #!/bin/bash
 
 USERID=$(id -u)
- R="\e[31m"
- G="\e[32m"
- Y="\e[33m"
- N="\e[0m"
+R="\e[31m"
+G="\e[32m"
+Y="\e[33m"
+N="\e[0m"
 
- LOGS_FOLDER="/var/log/shell-script"
- SCRIPT_NAME=$( echo $0 | cut -d "." -f1 )
- LOG_FILE="LOGS_FOLDER/SCRIPT_NAME.log"
- 
-mkdir -p $LOGS_FOLDER
-echo "script started executed at: $(date)"
+LOGS_FOLDER="/var/log/shell-script"
+SCRIPT_NAME=$(basename "$0" | cut -d "." -f1)
+LOG_FILE="$LOGS_FOLDER/$SCRIPT_NAME.log"
+
+mkdir -p "$LOGS_FOLDER"
+echo "Script started at: $(date)" | tee -a "$LOG_FILE"
+
 if [ "$USERID" -ne 0 ]; then
-    echo "ERROR:: Please run this script with root privilege"
-    exit 1 # failure is other than 0
+    echo -e "${R}ERROR:: Please run this script with root privilege${N}" | tee -a "$LOG_FILE"
+    exit 1
 fi
 
-VALIDATE(){ #VALIDATE $? "Mysql" passes two arguments to the function
-            #$1 = exit code $?
-            #$2 = package name (mysql/nginix/mongodb)etc..
-            #The function then uses these parameters to show appropriate messages
-     if [ $1 -ne 0 ]; then
-    echo -e "Installing $2 ... $R FAILURE $N"
-    exit 1
+VALIDATE(){
+    local exit_code=$1
+    local package_name=$2
+    
+    if [ "$exit_code" -ne 0 ]; then
+        echo -e "Installing $package_name ... ${R}FAILURE${N}" | tee -a "$LOG_FILE"
+        exit 1
     else
-    echo -e "Installing $2 ... $G SUCCESS $N"
+        echo -e "Installing $package_name ... ${G}SUCCESS${N}" | tee -a "$LOG_FILE"
     fi
 }
 
-dnf list installed mysql &>>$LOG_FILE
-# Install if it is not found
-if [ $? -ne 0 ]; then
-    dnf install mysql -y &>>$LOG_FILE
-VALIDATE $? "Mysql"
-else
-    echo -e "Mysql already exist ... $Y SKIPPING $N"
-fi    
+install_package() {
+    local package=$1
+    if dnf list installed "$package" &>>"$LOG_FILE"; then
+        echo -e "$package already exists ... ${Y}SKIPPING${N}" | tee -a "$LOG_FILE"
+    else
+        echo "Installing $package..." | tee -a "$LOG_FILE"
+        dnf install "$package" -y &>>"$LOG_FILE"
+        VALIDATE $? "$package"
+    fi
+}
 
-dnf list installed nginx &>>$LOG_FILE
-if [ $? -ne 0 ]; then
-    dnf install nginx -y &>>$LOG_FILE
-VALIDATE $? "nginx"
-else
-    echo -e "nginx already exist ... $Y SKIPPING $N"
-fi    
+# Install packages
+install_package "mysql"
+install_package "nginx" 
+install_package "python3"
 
-dnf list installed python3 &>>$LOG_FILE
-if [ $? -ne 0 ]; then
-    dnf install python3 -y &>>$LOG_FILE
-VALIDATE $? "python3"
-else
-    echo -e "python3 already exist ... $Y SKIPPING $N"
-fi
-
-
+echo "Script completed at: $(date)" | tee -a "$LOG_FILE"
