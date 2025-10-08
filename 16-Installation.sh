8cc2b734f1571a2cb6b3629057ad0601dc2f33f7
Kaladhars-Hub <kaladharcareers@gmail.com>
2025-10-08 18:17:48 +0530

    Installing mysql

--- 16-Installation.sh
+++ 16-Installation.sh
@@ -1,15 +1,17 @@
-!#/bin/bash
+#!/bin/bash
 
 USERID=$(id -u)
 
 if [$USERID -ne 0 ]; then
     echo "ERROR:: Please run this script with root privelage"
+    exit 1 #failure is other than 0
 fi
 
 dnf install mysql -y
 
 if [$? -ne 0 ]; then
     echo "ERROR:: Installing Mysql is failure"
+    exit 1
 else
     echo "Installing Mysql is SUCCESS"
 fi    
