c72fc84c48d94a8547203c49b20fb9b03c4f7a30
Kaladhars-Hub <kaladharcareers@gmail.com>
2025-10-23 14:41:32 +0530

    Learning Variables

---
+++ Learning Variables.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+#Variables are labeled boxes where we store information (Ingredients)
+
+#We Create a Variable by giving it a name and a value."No spaces around the = sign".
+
+#Example:1
+
+#Correct way of representing Variables.
+
+name="Alice"
+age=20
+
+#Wrong way of representing Variables.
+name = "Alice"
+
+#To get the value out of the box,we use the dollar sign "$" (Think of $ as "gimme the value").
+
+#Example
+echo "Hello, $name"
+echo "you are $age years old"
