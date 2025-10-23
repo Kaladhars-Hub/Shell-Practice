9e40693f182c9ff516b4d0302715e9df8845ab8d
Kaladhars-Hub <kaladharcareers@gmail.com>
2025-10-23 14:52:15 +0530

    Learning Variables

--- Learning Variables.sh
+++ Learning Variables.sh
@@ -12,10 +12,24 @@ name="Alice"
 age=20
 
 #Wrong way of representing Variables.
-name = "Alice"
+name = "Alice" #if we execute this command in terminal it will show as command not found,as it is wrong representation
 
 #To get the value out of the box,we use the dollar sign "$" (Think of $ as "gimme the value").
 
 #Example
 echo "Hello, $name"
 echo "you are $age years old"
+
+#Advanced type in Variables:Quoting
+
+#Double quotes:Use these most of the time,they let you use variables ($name) inside them.
+#Single quotes:These are literally same as the literal text "$name" not the value "Alice".
+
+#Command Substitution:Storing the o/p of a command in a variable.Use $(...).
+
+#Example
+current_date=$(date +% Y-% m-%d)
+echo "Today is current_date"
+
+
+
