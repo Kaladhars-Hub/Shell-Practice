6dfea19a18a63b5f18c1c08c9065530577f47713
Kaladhars-Hub <kaladharcareers@gmail.com>
2025-10-24 22:12:48 +0530

    Practicing for loop and while loop

--- whileloop.sh
+++ whileloop.sh
@@ -1,15 +1,5 @@
 #!/bin/bash
 
-# Count from 1 to 3
-count=1
-echo "Counting up:"
-while [ $count -le 3 ]; do
-    echo "Count: $count"
-    count=$((count + 1))
-done
-
-#!/bin/bash
-
 # Keep asking until user says yes
 answer=""
 while [ "$answer" != "yes" ]; do
