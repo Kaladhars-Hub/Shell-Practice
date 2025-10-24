71bb6b50b4a3048e689c89e66928ab50bba34935
Kaladhars-Hub <kaladharcareers@gmail.com>
2025-10-24 22:07:36 +0530

    Practicing for loop and while loop

--- whileloop.sh
+++ whileloop.sh
@@ -14,6 +14,16 @@ done
 password=""
 while [ "$password" != "hello" ]; do
     echo "Enter password:"
-    read password
+    read password #
 done
 echo "Access granted!"
+
+#!/bin/bash
+
+# Keep asking until user says yes
+answer=""
+while [ "$answer" != "yes" ]; do
+    echo "Are you ready? (yes/no)"
+    read answer
+done
+echo "Great! Let's go!"
