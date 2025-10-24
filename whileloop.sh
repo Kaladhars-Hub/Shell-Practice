762eaa1c253b4e6e3b7055f9fae4777e9f806662
Kaladhars-Hub <kaladharcareers@gmail.com>
2025-10-24 22:16:29 +0530

    Practicing for loop and while loop

--- whileloop.sh
+++ whileloop.sh
@@ -1,5 +1,25 @@
 #!/bin/bash
 
+# Count from 1 to 3
+count=1
+echo "Counting up:"
+while [ $count -le 3 ]; do
+    echo "Count: $count"
+    count=$((count + 1))
+done
+
+#!/bin/bash
+
+# Simple password check
+password=""
+while [ "$password" != "hello" ]; do
+    echo "Enter password:"
+    read password #
+done
+echo "Access granted!"
+
+#!/bin/bash
+
 # Keep asking until user says yes
 answer=""
 while [ "$answer" != "yes" ]; do
