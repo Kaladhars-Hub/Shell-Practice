5a5650dedff8d69f88686c07c593ef69a4bf1f1e
Kaladhars-Hub <kaladharcareers@gmail.com>
2025-10-24 16:47:09 +0530

    Practicing exit status

--- Exit status.sh
+++ Exit status.sh
@@ -9,3 +9,10 @@ echo $? #Prints Non Zero number
 #WE use this to control your script.the IF statement actually just checks the exit status.
 
 if [[ -f "/etc/hosts" ]]; then echo "File exists"; fi
+
+## You can use this to stop your script on failure
+cp sensitive.txt /backup/
+if [ $? -ne 0 ]; then
+  echo "FATAL: Backup failed! Stopping script."
+  exit 1 # Exit your *own* script with a "failure" score
+fi
