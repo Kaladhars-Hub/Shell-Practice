691f137ad68b4fa2256ea35b821836bb1f82b721
Kaladhars-Hub <kaladharcareers@gmail.com>
2025-10-24 16:33:01 +0530

    Practicing exit status

--- Exit status.sh
+++ Exit status.sh
@@ -4,4 +4,8 @@ ls /etc/hosts #This file exists
 echo $? #Prints 0 (Success)
 
 ls /not/a/real/file #This file doesnt exists
-echo $?
+echo $? #Prints Non Zero number
+
+#WE use this to control your script.the IF statement actually just checks the exit status.
+
+if [[ -f "/etc/hosts"]]; then ... fi
