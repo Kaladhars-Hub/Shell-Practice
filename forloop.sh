3d4389559a4ce39ed540b0a5b1a917daf607c951
Kaladhars-Hub <kaladharcareers@gmail.com>
2025-10-24 16:56:36 +0530

    Practicing for loop and while loop

--- forloop.sh
+++ forloop.sh
@@ -13,3 +13,12 @@ echo "My favorite fruits:"
 for fruit in apple banana orange mango; do
     echo "I like $fruit"
 done
+
+#!/bin/bash
+
+# Create 3 text files
+echo "Creating files:"
+for i in 1 2 3; do
+    touch "file$i.txt"
+    echo "Created file$i.txt"
+done
