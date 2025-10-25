3c74f38a60a257374302429b5a0bd3cb72ba8fa5
Kaladhars-Hub <kaladharcareers@gmail.com>
2025-10-25 19:58:31 +0530

    Learning redirections

--- Redirections.sh
+++ Redirections.sh
@@ -6,4 +6,11 @@ ls / > root_files.txt
 #Example:
 echo "Script ran at $(date)" >> script.log
 echo "Im learning Redirections" >> script.log
-echo "Im learning errors in redirections" 2> error_output.txt
+
+echo "Im learning errors in redirections"
+
+# Create actual errors and hide them
+ls /file_that_does_not_exist 2> error_output.txt
+cat /another_fake_file 2>> error_output.txt
+
+echo "Check the hidden errors:"
