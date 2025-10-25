4d03055ca426d3dee6383f4bf4e641c976d1de45
Kaladhars-Hub <kaladharcareers@gmail.com>
2025-10-25 19:49:33 +0530

    Learning redirections

--- Redirections.sh
+++ Redirections.sh
@@ -4,5 +4,6 @@
 ls / > root_files.txt
 
 #Example:
-echo "Script ran at $(date)" > script.log
-echo "Im learning Redirections" > script.log
+echo "Script ran at $(date)" >> script.log
+echo "Im learning Redirections" >> script.log
+echo Im learning errors in redirections 2>
