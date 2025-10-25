8e8165c2ad1e6b64db64b52c74da7ba11b32d04d
Kaladhars-Hub <kaladharcareers@gmail.com>
2025-10-25 19:37:26 +0530

    Learning functions

--- Functions.sh
+++ Functions.sh
@@ -6,14 +6,34 @@ greet () {
 greet
 greet
 #output
+
 # Hello,world!
 # Hello,world!
+
 #Advanced Example
 
 greet () {
     local person_type=$1
     local name=$2 $3
     echo "Hello, $name"
+}
+
+greet "person" "Barbie"
+greet "person" "Harvey"
+greet "person" "Superman"
+
+#Output
+
+Hello, Barbie
+Hello, Harvey
+Hello, Superman
+
+#Example:
+
+greet () {
+    # Skip the first argument "person" and use all others
+    shift
+    echo "Hello, $@"
 }
 
 greet "person" "Barbie"
