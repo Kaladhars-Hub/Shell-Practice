29f239d12773a6df3e207e18f9e54c34e8fa298e
Kaladhars-Hub <kaladharcareers@gmail.com>
2025-10-25 20:53:33 +0530

    Learning redirections

--- Redirections.sh
+++ Redirections.sh
@@ -1,3 +1,9 @@
 #!/bin/bash
 
-ls /etc/hosts /nonexistent_file > output.txt 2> /dev/null
+# Create a real file
+touch test_existing_file.txt
+
+# Now run the command
+ls test_existing_file.txt /this_file_does_not_exist > output.txt 2> /dev/null
+
+echo "Here's what got saved in output.txt:"
