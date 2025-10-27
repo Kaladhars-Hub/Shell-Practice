9fcc6fba2a4a9de22aabc7ff18357f0774b37fee
Kaladhars-Hub <kaladharcareers@gmail.com>
2025-10-27 17:27:02 +0530

    Practice logs

--- logs.sh
+++ logs.sh
@@ -2,3 +2,20 @@
 
 log_file= "/var/log/my_backup.log"
 echo "starting backup..." >> $log_file
+
+#Advanced
+
+#Create a logging function to make it cleaner and add a timestamps.
+
+log_file=script.log
+log(){
+    #Append the message ($1) to the log file with a time stamp.
+
+    echo[ $(date +'%Y-%m-%d%H:%M:%S')]-$1" >> $log_file
+}
+log "INFO:Script started."
+
+# ...do work...
+
+log"WARN:Low disk space"
+log"INFO:Script finished."
