97b04af787ec84607e8775a7a0a889bce38fcd18
Kaladhars-Hub <kaladharcareers@gmail.com>
2025-10-27 17:56:18 +0530

    Practicing Error Handling

--- Error Handling.sh
+++ Error Handling.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-rsync -a/my_data/backup/
-     if[ $? -ne 0 ]; then
-     echo "Error: rsync failed!"
-     exit 1
-     fi
+rsync -a /my_data /backup/
+if [ $? -ne 0 ]; then
+  echo "ERROR: rsync failed!"
+  exit 1
+fi
