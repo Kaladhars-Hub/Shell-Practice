bd225c3b0e652029444d066d720e0d6ab21769b9
Kaladhars-Hub <kaladharcareers@gmail.com>
2025-11-26 21:29:13 +0530

    Writing a script for cpu usage

---
+++ 25.disk-usage.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+DISK_USAGE=$(df -hT | grep -v Filesystem)
+DISK_THRESHOLD=2 # Keeping it low for testing
+IP_ADDRESS=$(curl http://169.254.169.254/latest/meta-data/local-ipv4 )
+MESSAGE=""
+# Loop through each line
+while IFS= read -r line; do
+    # Extract partition and usage
+    PARTITION=$(echo "$line" | awk '{print $1}')
+    USAGE=$(echo "$line" | awk '{print $6}' | cut -d'%' -f1)
+    
+    # Check threshold
+    if [ "$USAGE" -ge $DISK_THRESHOLD ]; then
+        # Send email alert (Fixed the quotes here)
+        MESSAGE="High Usage on: $PARTITION Usage: $USAGE %"
+    fi
+done <<< "$DISK_USAGE"
+
+echo "Message Body: $MESSAGE"
